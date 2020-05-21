96f90ff2b9cb4124f9d39a4abe367e9d35fca1f9
s1500729 <s1500729@sendai-nct.jp>
2020-05-21 21:40:43 +0900

    プロジェクト ファイルを追加します。

---
+++ ParrotSort/Main.cpp
@@ -0,0 +1,39 @@
+﻿# include <Siv3D.hpp>
+
+class Parrot
+{
+    Array<Image> parrots;
+    const int size = 10;
+    int index = 0;
+
+public:
+    Parrot(String path)
+    {
+        for (auto i : step(size))
+        {
+            parrots.push_back(Image(Format(path, i, ".png")));
+        }
+    }
+
+    void next()
+    {
+        index = (index + 1) % size;
+    }
+
+    Image current()
+    {
+        return parrots[index];
+    }
+};
+
+void Main()
+{
+    Parrot black(U"parrots/black"), pink(U"parrots/pink");
+
+    const Point pos(10, 90);
+
+    while (System::Update())
+    {
+        
+    }
+}
