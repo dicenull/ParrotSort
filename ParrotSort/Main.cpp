2f0938fea637bfe5e7b43ae710aca388769b3621
s1500729 <s1500729@sendai-nct.jp>
2020-05-21 21:56:49 +0900

    10fpsで画像を動かすようにしtあ

--- ParrotSort/Main.cpp
+++ ParrotSort/Main.cpp
@@ -1,26 +1,54 @@
 ﻿# include <Siv3D.hpp>
 
+class Counter
+{
+    int count, max;
+    bool refresh = false;
+
+public:
+    Counter(int max)
+    {
+        this->max = max;
+    }
+
+    void increment()
+    {
+        count++;
+        count %= max;
+
+        refresh = (count == 0);
+    }
+
+    bool getRefresh() { return refresh; }
+};
+
 class Parrot
 {
-    Array<Image> parrots;
+    Array<Texture> parrots;
     const int size = 10;
     int index = 0;
+    Counter counter{ 6 };
 
 public:
     Parrot(String path)
     {
         for (auto i : step(size))
         {
-            parrots.push_back(Image(Format(path, i, ".png")));
+            parrots.push_back(Texture(Format(path, i, U".png")));
         }
     }
 
     void next()
     {
-        index = (index + 1) % size;
+        counter.increment();
+
+        if (counter.getRefresh())
+        {
+            index = (index + 1) % size;
+        }
     }
 
-    Image current()
+    Texture current()
     {
         return parrots[index];
     }
@@ -28,12 +56,16 @@ public:
 
 void Main()
 {
-    Parrot black(U"parrots/black"), pink(U"parrots/pink");
+    Parrot black(U"parrots/black/black"), pink(U"parrots/pink/pink");
 
     const Point pos(10, 90);
 
     while (System::Update())
     {
-        
+        black.current().draw(pos);
+        pink.current().draw(Point(100, 200));
+
+        black.next();
+        pink.next();
     }
 }
