d1380f98107d104355539f8a7277fa6bfd61c9ed
s1500729 <s1500729@sendai-nct.jp>
2020-05-25 22:57:56 +0900

    parrotをしまっておく領域を追加

--- ParrotSort/Main.cpp
+++ ParrotSort/Main.cpp
@@ -10,27 +10,37 @@ void Main()
     
     Scene::SetBackground(Palette::Gray);
 
-    
     manager.generate(ParrotColor::Default);
-    for (auto i : step(100))
+    for (auto i : step(10))
     {
         manager.generate(ParrotColor(Random(1)));
     }
-    
+
     bool start = false;
+
+    const auto size = Size(200, 200);
+    const auto padding = Point(10, 200);
+    Rect pink_area{ padding, size };
+    Rect black_area{ Point(Scene::Width() - padding.x - size.x, padding.y), size };
 
     while (System::Update())
     {
-        if (start)
-        {
-            manager.draw();
-
-            manager.update();
-        }
-
         if (KeySpace.down())
         {
             start = true;
+        }
+
+        if (start)
+        {
+            pink_area.draw(Palette::Pink);
+            black_area.draw(Palette::Darkgray);
+
+            manager.draw();
+
+            manager.update();
+
+            manager.checkArea(pink_area);
+            manager.checkArea(black_area);
         }
     }
 
